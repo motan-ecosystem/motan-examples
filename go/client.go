bf5c532c9ec259358657ed18a4b400dc1702077b
zhanglei28 <zhanglei28@staff.weibo.com>
2018-05-21 19:57:18 +0800

    adapt multi params

--- go/client.go
+++ go/client.go
@@ -21,7 +21,7 @@ func runClientDemo() {
 	args["crop"] = "weibo"
 	args["site"] = "idevz.org"
 	var reply string
-	err := mclient.Call("Hello", args, &reply)
+	err := mclient.Call("Hello", []interface{}{args}, &reply)
 	if err != nil {
 		fmt.Printf("motan call fail! err:%v\n", err)
 	} else {
@@ -31,7 +31,7 @@ func runClientDemo() {
 	bigIntBytes:=new(bytes.Buffer)
 	bigInt:=uint64(0xffffffffffffffff)
 	binary.Write(bigIntBytes, binary.BigEndian, bigInt)
-	if err := mclient.Call("BigInt", bigIntBytes.String(), &reply); err == nil {
+	if err := mclient.Call("BigInt", []interface{}{bigIntBytes.String()}, &reply); err == nil {
 		fmt.Printf("motan call success! reply:%s\n", reply)
 	} else {
 		fmt.Printf("motan call fail! err:%v\n", err)
@@ -39,7 +39,7 @@ func runClientDemo() {
 
 	// async call
 	args["is-async"] = "yes --->>>"
-	result := mclient.Go("Hello", args, &reply, make(chan *motancore.AsyncResult, 1))
+	result := mclient.Go("Hello", []interface{}{args}, &reply, make(chan *motancore.AsyncResult, 1))
 	res := <-result.Done
 	if res.Error != nil {
 		fmt.Printf("motan async call fail! err:%v\n", res.Error)
